ebe5ff7944828f49d07f108f6ee24d0be8c0eddb
Charli <cm32brown@gmail.com>
2022-05-19 14:19:54 -0600

    Create Boot MVC project, add JPA project dependencies

---
+++ InterDistillery/src/main/java/com/skilldistillery/interdistillery/InterDistilleryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.interdistillery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterDistilleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterDistilleryApplication.class, args);
+	}
+
+}
---
+++ InterDistillery/src/test/java/com/skilldistillery/interdistillery/InterDistilleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.interdistillery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterDistilleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
