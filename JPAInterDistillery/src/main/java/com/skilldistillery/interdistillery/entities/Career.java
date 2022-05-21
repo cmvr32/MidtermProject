a19ed001ef5d3956c7aa6b5306d0466259b6e038
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-05-21 09:53:33 -0600

    junit test user, skill, and resuume

--- JPAInterDistillery/src/main/java/com/skilldistillery/interdistillery/entities/Career.java
+++ JPAInterDistillery/src/main/java/com/skilldistillery/interdistillery/entities/Career.java
@@ -14,14 +14,19 @@ public class Career {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
 	@Column(name = "career_title")
 	private String careerTitle;
+	
 	@Column(name = "image_url")
 	private String imageUrl;
+	
 	@Column(name = "low_salary")
 	private double lowSalary;
+	
 	@Column(name = "high_salary")
 	private double highSalary;
+	
 	@Column(name = "career_category_id")
 	private int careerCategoryId;
 
