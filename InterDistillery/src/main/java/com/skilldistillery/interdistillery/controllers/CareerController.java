85eb81d438b10c2bcacdb57596ed1bdf85d13a71
Angel <angel1179008@gmail.com>
2022-05-23 14:09:35 -0600

    controllers started

--- InterDistillery/src/main/java/com/skilldistillery/interdistillery/controllers/CareerController.java
+++ InterDistillery/src/main/java/com/skilldistillery/interdistillery/controllers/CareerController.java
@@ -1,5 +1,95 @@
 package com.skilldistillery.interdistillery.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.skilldistillery.interdistillery.data.CareerDAO;
+import com.skilldistillery.interdistillery.entities.Career;
+
 public class CareerController {
+
+	@Autowired
+	private CareerDAO careerdao;
+
+	@RequestMapping(path = { "/", "home.do" })
+	public String home(Model model) {
+		model.addAttribute("DEBUG", careerdao.findById(1)); // DEBUG REMOVE LATER
+		return "homePage";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String singleCareer(int id, Model model) {
+		List<Career> careers = new ArrayList<>();
+		careers.add(careerdao.findById(id));
+		model.addAttribute("careers", careers);
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String getAllCareers(Model model) {
+		List<Career> careers = careerdao.findAllCareers();
+		model.addAttribute("careers", careers);
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String addCareer(RedirectAttributes redir, Career careers) {
+		careers = careerdao.addNewCareer(careers);
+		boolean addSpendingFlag = true;
+		redir.addFlashAttribute("addCareerFlag", addCareerFlag);
+		redir.addFlashAttribute("careers", careers);
+		return "redirect:.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String addCareerGetProcess(Career career) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String addNewCareer() {
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String deleteCareer(RedirectAttributes redir, int id) {
+		boolean containsFlag = careerdao.deleteCareer(id);
+		boolean deleteCareerFlag = true;
+		redir.addFlashAttribute("deleteCareerFlag", deleteCareerFlag);
+		redir.addFlashAttribute("containsFlag", containsFlag);
+		return "redirect:deleteSpending.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String deleteCareerGetProcess(Career career) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String updateCareer(RedirectAttributes redir, Career career) {
+		boolean updateCareerFlag = true;
+		career = careerdao.updateCareer(career);
+		redir.addFlashAttribute("updateCareerFlag", updateCareerFlag);
+		redir.addFlashAttribute("career", career);
+		return "redirect:.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String updateCareerGetProcess(Career career) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String updateCareerPage(Model model, int id) {
+		Career career = careerdao.findById(id);
+		model.addAttribute("career", career);
+		return "";
+	}
 
 }
--- InterDistillery/src/main/java/com/skilldistillery/interdistillery/controllers/MockInterviewController.java
+++ InterDistillery/src/main/java/com/skilldistillery/interdistillery/controllers/MockInterviewController.java
@@ -1,5 +1,96 @@
 package com.skilldistillery.interdistillery.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.skilldistillery.interdistillery.data.MockInterviewDAO;
+import com.skilldistillery.interdistillery.entities.Career;
+import com.skilldistillery.interdistillery.entities.MockInterview;
+
 public class MockInterviewController {
+
+	@Autowired
+	private MockInterviewDAO mockInterviewDao;
+
+	@RequestMapping(path = { "/", "home.do" })
+	public String home(Model model) {
+		model.addAttribute("DEBUG", mockInterviewDao.findById(1)); // DEBUG REMOVE LATER
+		return "homePage";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String singleInterview(int id, Model model) {
+		List<MockInterview> interviews = new ArrayList<>();
+		interviews.add(mockInterviewDao.findById(id));
+		model.addAttribute("interviews", interviews);
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String getAllInterview(Model model) {
+		List<MockInterview> interviews = mockInterviewDao.findAllInterviews();
+		model.addAttribute("interviews", interviews);
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String addInterview(RedirectAttributes redir, MockInterview interviews) {
+		interviews = mockInterviewDao.addNewCareer(interviews);
+		boolean addInterviewFlag = true;
+		redir.addFlashAttribute("addInterviewsFlag", addInterviewFlag);
+		redir.addFlashAttribute("careers", interviews);
+		return "redirect:.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String addInterviewsGetProcess(MockInterview interviews) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String addNewInterview() {
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String deleteInterview(RedirectAttributes redir, int id) {
+		boolean containsFlag = mockInterviewDao.deleteInterview(id);
+		boolean deleteInterviewFlag = true;
+		redir.addFlashAttribute("deleteInterviewFlag", deleteInterviewFlag);
+		redir.addFlashAttribute("containsFlag", containsFlag);
+		return "redirect:.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String deleteInterviewGetProcess(MockInterview interviews) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.POST)
+	public String updateInterview(RedirectAttributes redir, MockInterview interviews) {
+		boolean updateInterviewFlag = true;
+		interviews = mockInterviewDao.updateInterview(interviews);
+		redir.addFlashAttribute("updateInterviewFlag", updateInterviewFlag);
+		redir.addFlashAttribute("career", interviews);
+		return "redirect:.do";
+	}
+
+	@RequestMapping(path = ".do", method = RequestMethod.GET)
+	public String updateInterviewGetProcess(MockInterview interviews) {
+		return "";
+	}
+
+	@RequestMapping(path = ".do")
+	public String Interview(Model model, int id) {
+		MockInterview interviews = mockInterviewDao.findById(id);
+		model.addAttribute("interviews", interviews);
+		return "";
+	}
 
 }
