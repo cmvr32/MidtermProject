4ed2115bcb56c5ae594732b3a0ec8751f71461c8
Nathan Hafley <95002066+Nateomancer@users.noreply.github.com>
2022-05-23 18:41:03 -0600

    finished MockInterview DAO

--- InterDistillery/src/main/java/com/skilldistillery/interdistillery/data/MockInterviewDAO.java
+++ InterDistillery/src/main/java/com/skilldistillery/interdistillery/data/MockInterviewDAO.java
@@ -1,10 +1,25 @@
 package com.skilldistillery.interdistillery.data;
 
+import java.util.List;
+
 import com.skilldistillery.interdistillery.entities.MockInterview;
 
 public interface MockInterviewDAO {
 	
-	MockInterview findById(int userId);
-	MockInterview findByUserIdAndMockInterviewId(int userId, int mockInterviewId);
+	public MockInterview findByInterviewId(int interviewId);
+	
+	public List<MockInterview> listAllInterviews();
+	
+	public MockInterview findByMockIntervieweeId(int intervieweeId);
+	
+	public List<MockInterview> findByUserIdAndMockInterviewId(int userId, int mockInterviewId);
+	
+	public MockInterview createInterview(MockInterview mockInterview);
+
+	public boolean deleteInterview(int interviewId);
+
+	
+	
+	
 
 }
